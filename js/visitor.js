b4ee67780cf1274c4e820fe026335274d8ac7dad
OkHyeon <usdrd90@gmail.com>
2015-11-07 11:29:50 +0900

    modal show hide

--- js/visitor.js
+++ js/visitor.js
@@ -1,13 +1,40 @@
 
 $(function(){
-
+	visitor.init();
 });
 
-function visitor() {
-	
-};
 
-var visitors = {};
+var visitor = {
+	$el : {},
+
+	init : function(){
+		var _ = this;
+		
+
+		$('.view-modal.btn').click(function(e){
+			e.preventDefault();
+			_.showModal();
+		});
+
+		$('.close.btn ,#btnClose').click(function(e){
+			e.preventDefault();
+			_.closeModal();
+		});	
+	},
+
+	showModal : function(){
+		var _ = this;
+		console.log('aa')
+		$('#visitorModal').css('display','block')		
+	},
+	
+	closeModal : function(){
+		var _ = this;
+		$('#visitorModal').css('display','none')	
+	},
+
+
+};
 
 
 
